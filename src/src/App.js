a813d7e70c3efd89be42e8825d5587013d87b4d2
Ashley Salinas <ashleysalinas@ashleys-mbp.lan>
2023-06-14 22:42:31 -0500

    added twitter integration

--- src/src/App.js
+++ src/src/App.js
@@ -1,3 +1,4 @@
+import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 import QuoteCard from './components/quote-card';
--- src/src/components/quote-card.js
+++ src/src/components/quote-card.js
@@ -1,9 +1,13 @@
+import React from "react";
+
 const QuoteCard = ({quote}) => {
+    let url = 'https://twitter.com/intent/tweet?text=' + '"' + quote.quote + '"' + '-' + quote.author;
     return (
         <div>
             <h2>This is the quote card</h2>
             <h3>{quote.quote}</h3>
             <h3>{quote.author}</h3>
+            <a href={url}>Tweet this</a>
         </div>
     )
 }
